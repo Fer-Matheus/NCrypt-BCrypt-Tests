8f398e96deba2366e48e06c183489557a86b8a56
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2023-10-27 12:59:03 -0300

    Update all the outputs to be clearer

--- SignFlow/SignFlow.cpp
+++ SignFlow/SignFlow.cpp
@@ -14,26 +14,50 @@
 #pragma comment(lib, "crypt32.lib")
 
 Type type = Type::RSA;
+Alg alg = Alg::SHA1;
 
-std::vector<unsigned char> Hash(const wchar_t* data) {
+// Functions declaration
+std::vector<unsigned char> Hash(const wchar_t *data);
+void Sign(const wchar_t *keyName, std::vector<unsigned char> digest);
+const wchar_t *CreateKey();
+
+int main()
+{
+
+	auto keyName = CreateKey();
+
+	auto digest = Hash(L"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste facere nobis voluptatibus at similique, qui iure deserunt perferendis fugiat vitae asperiores reprehenderit dignissimos itaque consectetur voluptatum sed alias nemo aliquid.");
+
+	Sign(keyName, digest);
+
+	return 0;
+}
+
+std::vector<unsigned char> Hash(const wchar_t *data)
+{
 	BCRYPT_ALG_HANDLE algHandle;
-
 	NTSTATUS status;
 
-	status = BCryptOpenAlgorithmProvider(&algHandle, BCRYPT_SHA256_ALGORITHM, NULL, 0);
-	Error("Open Algorithm\n", status);
+	TITLE("Start digest generating process using bcrypt library");
+
+	auto algorithm = ENUM::GetAlg(alg);
+
+	status = BCryptOpenAlgorithmProvider(&algHandle, algorithm, NULL, 0);
+	ERROR("Open Algorithm " << ENUM::GetAlgName(alg), status);
 
 	DWORD size = 0;
 
 	status = BCryptGetProperty(algHandle, BCRYPT_HASH_LENGTH, (PBYTE)&size, sizeof(size), &size, 0);
-	Error("Get property\n", status);
+	ERROR("Get hash length property", status);
 
 	std::vector<unsigned char> digest(size);
 
 	status = BCryptHash(algHandle, NULL, 0, (PBYTE)data, sizeof(data), digest.data(), size);
-	Error("Creating digest\n", status);
+	ERROR("Creating digest", status);
 
-	for (int i = 0; i < digest.size(); i++) {
+	LOG("Digest: ")
+	for (int i = 0; i < digest.size(); i++)
+	{
 		printf("%02x", digest[i]);
 	}
 	LOG("\n");
@@ -43,83 +67,83 @@ std::vector<unsigned char> Hash(const wchar_t* data) {
 	return digest;
 }
 
-void CreateKey() {
+const wchar_t *CreateKey()
+{
 
 	NCRYPT_PROV_HANDLE pHandle;
 	NCRYPT_KEY_HANDLE keyHandle;
 
-	auto status = NCryptOpenStorageProvider(&pHandle, MS_PLATFORM_CRYPTO_PROVIDER, 0);
-	Error("Open Storage Provider", status);
+	auto keyName = L"KeyTest";
 
-	status = NCryptCreatePersistedKey(pHandle, &keyHandle, (type == Type::RSA ? BCRYPT_RSA_ALGORITHM : BCRYPT_ECDSA_ALGORITHM), L"Mamaco", 0, NCRYPT_OVERWRITE_KEY_FLAG);
-	Error("Create Key", status);
+	TITLE("Start a create key process");
+
+	auto status = NCryptOpenStorageProvider(&pHandle, MS_PLATFORM_CRYPTO_PROVIDER, 0);
+	ERROR("Open storage provider (PCP)", status);
+
+	status = NCryptCreatePersistedKey(pHandle, &keyHandle, (type == Type::RSA ? BCRYPT_RSA_ALGORITHM : BCRYPT_ECDSA_ALGORITHM), keyName, 0, NCRYPT_OVERWRITE_KEY_FLAG);
+	ERROR("Creating a RSA key", status);
 
 	DWORD keyUsage = NCRYPT_ALLOW_SIGNING_FLAG;
 
 	status = NCryptSetProperty(keyHandle, NCRYPT_KEY_USAGE_PROPERTY, (PBYTE)&keyUsage, sizeof(keyUsage), NCRYPT_PERSIST_FLAG);
-	Error("Set Property", status);
+	ERROR("Set a allow signing property", status);
 
-	if (NCryptIsKeyHandle(keyHandle)) {
+	if (NCryptIsKeyHandle(keyHandle))
+	{
 		LOG("Valid!\n");
-	}
-	else {
-		LOG("Not Valid!\n")
-			exit(-1);
 	}
 
 	status = NCryptFinalizeKey(keyHandle, 0);
-	Error("Finalize Key", status);
+	ERROR("Finalize Key", status);
 
 	NCryptFreeObject(keyHandle);
 	NCryptFreeObject(pHandle);
+	return keyName;
 }
 
-void Sign() {
+void ShowKey(NCRYPT_KEY_HANDLE keyHandle)
+{
+	DWORD size = 0;
+	auto status = NCryptGetProperty(keyHandle, NCRYPT_NAME_PROPERTY, NULL, NULL, &size, 0);
+	ERROR("First Get key name property", status);
+}
+
+void Sign(const wchar_t *keyName, std::vector<unsigned char> digest)
+{
 	NCRYPT_PROV_HANDLE pHandle;
 	NCRYPT_KEY_HANDLE keyHandle;
 
+	TITLE("Start a sign digest process using ncrypt library");
+
 	auto status = NCryptOpenStorageProvider(&pHandle, MS_PLATFORM_CRYPTO_PROVIDER, 0);
-	Error("Open Storage Provider\n", status);
+	ERROR("Open Storage Provider (PCP)", status);
 
-	status = NCryptOpenKey(pHandle, &keyHandle, L"Mamaco", 0, 0);
-	Error("Open Key\n", status);
-
-	if (NCryptIsKeyHandle(keyHandle)) {
-		LOG("Valid!\n");
-	}
-
-	auto data = L"Mamaco";
-
-	auto digest = Hash(data);
+	status = NCryptOpenKey(pHandle, &keyHandle, keyName, 0, 0);
+	ERROR("Open a RSA key storage on the file system", status);
 
 	DWORD size = 0;
 
 	BCRYPT_PKCS1_PADDING_INFO info;
-	info.pszAlgId = (type == Type::RSA ? BCRYPT_SHA256_ALGORITHM : BCRYPT_ECDSA_P256_ALGORITHM);
 
-	auto padding = (type == Type::RSA ? BCRYPT_PAD_PKCS1 : 0);
+	info.pszAlgId = ENUM::GetAlg(alg);
 
-	status = NCryptSignHash(keyHandle, &info, digest.data(), digest.size(), NULL, NULL, &size, padding);
-	Error("First SignHash\n", status);
+	status = NCryptSignHash(keyHandle, &info, digest.data(), digest.size(), NULL, NULL, &size, BCRYPT_PAD_PKCS1);
+	ERROR("First SignHash", status);
 
 	std::vector<unsigned char> signature(size);
 
-	status = NCryptSignHash(keyHandle, &info, digest.data(), digest.size(), signature.data(), size, &size, padding);
-	Error("Second SignHash\n", status);
+	status = NCryptSignHash(keyHandle, &info, digest.data(), digest.size(), signature.data(), size, &size, BCRYPT_PAD_PKCS1);
+	ERROR("Second SignHash", status);
 
 	LOG("Signature: \n");
-	FOR(size) printf("%02x", signature[i]);
+	FOR(size)
+	printf("%02x", signature[i]);
+	LOG("\n");
 	LOG("\n");
 
-	status = NCryptVerifySignature(keyHandle, &info, digest.data(), digest.size(), signature.data(), signature.size(), padding);
-	Error("Verify Signature\n", status);
+	status = NCryptVerifySignature(keyHandle, &info, digest.data(), digest.size(), signature.data(), signature.size(), BCRYPT_PAD_PKCS1);
+	ERROR("Verify Signature", status);
 
 	NCryptFreeObject(keyHandle);
 	NCryptFreeObject(pHandle);
-}
-
-int main() {
-	Sign();
-	//CreateKey();
-	return 0;
 }
