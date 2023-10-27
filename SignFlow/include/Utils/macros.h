7c3341ec52b52d9d6605b838df80a0a8b17b8195
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2023-10-27 12:56:31 -0300

    Improve some macros and add an algorithm and a keyType enum

--- SignFlow/include/Utils/macros.h
+++ SignFlow/include/Utils/macros.h
@@ -2,13 +2,15 @@
 
 #define LOG(message) std::cout << message;
 
-#define Error(stage, code)\
+#define TITLE(message) LOG("\n\t>> " << message << "...\n\n")
+
+#define ERROR(stage, code)\
 	if (code != 0){\
 		LOG(stage << " fail\n");\
 		LOG("Error code: " << std::hex << code << "\n");\
 		exit(-1);\
 	}else{\
-		LOG(stage << " OK\n");\
+		LOG(stage << ": OK\n\n");\
 	}\
 
 #define FOR(size) for(int i = 0; i < size; i++)
--- SignFlow/include/enum.h
+++ SignFlow/include/enum.h
@@ -5,3 +5,33 @@ enum Type
 	RSA,
 	ECC
 };
+enum Alg {
+	SHA1,
+	SHA256,
+};
+
+namespace ENUM {
+	const wchar_t* GetAlg(Alg alg) {
+		switch (alg)
+		{
+		case SHA1:
+			return BCRYPT_SHA1_ALGORITHM;
+		case SHA256:
+			return BCRYPT_SHA256_ALGORITHM;
+			break;
+		default:
+			break;
+		}
+	}
+	std::string GetAlgName(Alg alg) {
+		switch (alg)
+		{
+		case SHA1:
+			return "SHA1";
+		case SHA256:
+			return "SHA256";
+		default:
+			break;
+		}
+	}
+}
